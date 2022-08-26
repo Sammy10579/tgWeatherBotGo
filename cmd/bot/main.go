19b75b4e288af44b03edc164ef218e816f6442cd
Sammy10579 <ramenskiivlad@gmail.com>
2022-08-26 16:17:41 +0300

    start bot

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -17,8 +17,6 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
@@ -27,7 +25,7 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
+		if update.Message != nil {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
